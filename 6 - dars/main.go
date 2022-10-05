aafef68a5b6c6bf7a2b9bdd8cb6449377e0d52a8
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-05 21:23:25 +0500

    Golang Algoritmlari

--- 6 - dars/main.go
+++ 6 - dars/main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	fmt.Println("Hello, playground")
+	fmt.Println(function2(2003))
+
+}
+
+func function(x float64) int {
+	return int(math.Pow(x, 2003))
+}
+
+func function1(x int) int {
+	return x * x
+}
+
+func function2(x int) int {
+	return x * x * x
 }
