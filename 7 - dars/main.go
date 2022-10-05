13c69ad858abbb3e5283b3713dedb6cc2c35521e
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-05 21:45:07 +0500

    Golang Algoritmlari

--- 7 - dars/main.go
+++ 7 - dars/main.go
@@ -10,10 +10,17 @@ type user struct {
 	Email     string
 	Password  string
 	Age       int
+	Engine   engine
+}
+
+type engine struct {
+	Name string
+	Hp int
 }
 
 func main() {
-	users := user{FirstName: "John", LastName: "Doe", Email: "dev.dilshojdon@gmail.com", Password: "123456", Age: 20}
+	users := user{FirstName: "John", LastName: "Doe", Email: "dev.dilshojdon@gmail.com", 
+	Password: "123456", Age: 20,Engine: engine{Name: "V8",Hp: 500}}
 	fmt.Println(users)
 	fmt.Println(users.FirstName)
 }
