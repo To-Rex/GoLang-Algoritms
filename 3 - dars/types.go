85227a76a58a33ec7be1bc57c22bcbdfd9156714
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-05 16:09:01 +0500

    Golang Algoritmlari

--- 3 - dars/types.go
+++ 3 - dars/types.go
@@ -1,16 +1,7 @@
 //constructor users
 package main
 
-func NewUser(name string, age int) *User {
-	return &User{
-		Name: name,
-		Age:  age,
-	}
-}
-//constructor users
-func NewUser(name string, age int) *User {
-	return &User{
-		Name: name,
-		Age:  age,
-	}
+type users struct {
+	name string
+	age  int
 }
