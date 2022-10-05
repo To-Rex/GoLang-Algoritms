aaaaf0fcbf265642d88c7eef70710e20ced72f97
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-05 20:43:09 +0500

    Golang Algoritmlari

--- 4 - dars/main.go
+++ 4 - dars/main.go
@@ -5,5 +5,68 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World")
+	maps1()
+}
+
+func maps() {
+	//create map
+	var map1 map[string]int
+	map1 = make(map[string]int)
+	map1["a"] = 1
+	map1["b"] = 2
+	map1["c"] = 3
+	map1["d"] = 4
+	map1["e"] = 5
+	map1["f"] = 6
+	map1["g"] = 7
+	map1["h"] = 8
+
+	fmt.Println(map1)
+}
+
+func maps1() {
+	//create map
+	map1 := make(map[string]int)
+	map1["a"] = 1
+	map1["b"] = 2
+	map1["c"] = 3
+	map1["d"] = 4
+	map1["e"] = 5
+	map1["f"] = 6
+	map1["g"] = 7
+	map1["h"] = 8
+
+	fmt.Println(map1)
+}
+
+func maps2() {
+	//create map
+	map1 := map[string]int{
+		"a": 1,
+		"b": 2,
+		"c": 3,
+		"d": 4,
+		"e": 5,
+		"f": 6,
+		"g": 7,
+		"h": 8,
+	}
+
+	fmt.Println(map1)
+}
+
+func maps3() {
+	//create map
+	map1 := map[string]int{
+		"a": 1,
+		"b": 2,
+		"c": 3,
+		"d": 4,
+		"e": 5,
+		"f": 6,
+		"g": 7,
+		"h": 8,
+	}
+
+	fmt.Println(map1["a"])
 }
