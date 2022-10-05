939e1999a4008df36edc5e481ca23887b2c7c409
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-05 22:00:46 +0500

    Golang Algoritmlari

--- 8 - dars/main.go
+++ 8 - dars/main.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+	"fmt"
+)
+
+type queue struct {
+	items []int
+}
+
+func (q *queue) enqueue(i int) {
+	q.items = append(q.items, i)
+}
+
+func (q *queue) dequeue() int {
+	toRemove := q.items[0]
+	q.items = q.items[1:]
+	return toRemove
+}
+
+func (q *queue) isEmpty() bool {
+	return len(q.items) == 0
+}
+
+func (q *queue) peek() int {
+	return q.items[0]
+}
+
+func (q *queue) print() {
+	fmt.Println(q.items)
+}
+
+func (q *queue) size() int {
+	return len(q.items)
+}
+
+
 func main() {
-	
+	q := queue{}
+	q.enqueue(1)
+	q.enqueue(2)
+	q.enqueue(3)
+	q.enqueue(4)
+	q.print()
+	fmt.Println(q.dequeue())
+	q.print()
+	fmt.Println(q.peek())
+	q.print()
+	fmt.Println(q.size())
+	q.print()
+	fmt.Println(q.isEmpty())
+	q.print()
 }
