dd2e627d0172cc0f03ae1b8dfb9b2d11c4d2a63c
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-05 20:48:49 +0500

    Golang Algoritmlari

--- 5 - dars/main.go
+++ 5 - dars/main.go
@@ -5,5 +5,30 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, playground")
+	loop()
+}
+
+func loop() {
+	for i := 0; i < 10; i++ {
+		fmt.Print(i)
+	}
+}
+
+func loop1() {
+	i := 0
+	for i < 10 {
+		fmt.Print(i)
+		i++
+	}
+}
+
+func loop2() {
+	i := 0
+	for {
+		fmt.Print(i)
+		i++
+		if i == 10 {
+			break
+		}
+	}
 }
