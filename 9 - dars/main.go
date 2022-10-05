ba93a66b8b44b2466d473a116350d5744559281d
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-05 22:51:16 +0500

    Golang Algoritmlari

--- 9 - dars/main.go
+++ 9 - dars/main.go
@@ -3,18 +3,45 @@ package main
 import (
 	"fmt"
 	"time"
+	"sync"
 )
 
 func main() {
-	go display("salom")
-	go display("hello")
-	fmt.Scanln()
+	start1()
+}
+func start() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go display("salom", &wg)
+	go display("hello", &wg)
+	wg.Wait()
 }
 
-func display(input string) {
-	for i := 0; true; i++ {
+func start1() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func ()  {
+		display1("salom")
+		wg.Done()
+	}()
+	go func ()  {
+		display1("hello")
+		wg.Done()
+	}()
+	wg.Wait()
+}
+
+func display(input string, wg *sync.WaitGroup) {
+	for i := 0; i<=5; i++ {
 		fmt.Println(i, input)
 		time.Sleep(1 * time.Second)
-	
+	}
+	wg.Done()
+}
+
+func display1(input string) {
+	for i := 0; i<=5; i++ {
+		fmt.Println(i, input)
+		time.Sleep(1 * time.Second)
 	}
 }
