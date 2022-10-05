ec83a8577a08c3902a47c5bd62498cfa3d22050f
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-05 21:03:35 +0500

    Golang Algoritmlari

--- 5 - dars/main.go
+++ 5 - dars/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	loop3()
+	loop5()
 }
 
 func loop() {
@@ -38,5 +38,32 @@ func loop3() {
 	for {
 		time.Sleep(1 * time.Second)
 		fmt.Println(time.Now().Second())
+	}
+}
+
+func loop4() {
+	mArr := [5]int{1, 2, 3, 4, 5}
+	for i, v := range mArr {
+		fmt.Println(i, v)
+	}
+
+
+}
+func loop5() {
+	mMap := map[string]int{
+		"a": 1,
+		"b": 2,
+		"c": 3,
+		"d": 4,
+		"e": 5,
+		"f": 8,
+		"g": 7,
+		"h": 6,
+		"i": 5,
+		"j": 4,
+	}
+
+	for k, v := range mMap {
+		fmt.Println(k, v)
 	}
 }
