a2b21dd32c48fea91cb5d03af17b9473b87947b0
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-10-05 15:48:50 +0500

    Golang Algoritmlari

--- 2 - dars/main.go
+++ 2 - dars/main.go
@@ -4,5 +4,11 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World")
+	mArr :=[...][]int{{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16},{17,18,19,20},{21,22,23,24}}
+	for _,v:=range mArr{
+		fmt.Println(v)
+		for _,v1:=range v{
+			fmt.Println(v1)
+		}
+	}
 }
